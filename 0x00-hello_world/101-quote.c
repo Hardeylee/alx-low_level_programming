290f177d3cdd341908d4ee4ce41b4042326c8f47
Hardeylee <hardeyinkahadekunle@gmail.com>
2022-09-08 13:28:58 -0700

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
 /**
  * main - main block
  * Return: 1
@@ -8,7 +7,6 @@
 int main(void)
 {
 	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	
 	write(2,
 			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 			s);
