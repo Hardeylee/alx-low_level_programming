fe551d82893f9cbbe386fcd92f31d4e856d9a002
Hardeylee <hardeyinkahadekunle@gmail.com>
2022-09-09 07:07:52 -0700

    102 print comb

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -25,10 +25,10 @@ int main(void)
 						putchar(k);
 						putchar(j);
 
-						if (c + i + k + j == 227 && c == 57)
+					if (c + i + k + j == 227 && c == 57)
 						{
 							break;
-						}
+					}
 						else
 						{
 						putchar(',');
